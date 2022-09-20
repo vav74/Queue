ebf6092063f7f021fdeb6334d203da1fe72d0829
vav74 <111101156+vav74@users.noreply.github.com>
2022-09-20 16:46:51 +0900

    2-nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,29 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        List<Person> crowd = generateClients();
+        Queue<Person> visitorsQueue = new LinkedList<>(crowd);
+        while (!visitorsQueue.isEmpty()) {
+            Person currentVisitor = visitorsQueue.poll();
+            currentVisitor.setNumTickets(currentVisitor.getNumTickets() - 1);
+            System.out.println(currentVisitor.getName() + " прокатился на аттракционе (осталось " + currentVisitor.getNumTickets() + " билетов)");
+            if (currentVisitor.getNumTickets() != 0) {
+                visitorsQueue.offer(currentVisitor);
+            }
+        }
     }
 
     public static List<Person> generateClients() {
         List<Person> list = new ArrayList<>();
-        Person Person1 = new Person("Борис Васильев", 10);
-        Person Person2 = new Person("Александра Булкина", 6);
-        Person Person3 = new Person("Наталья Портамонова", 5);
-        Person Person4 = new Person("Емельян Макаронов", 7);
-        Person Person5 = new Person("Иннокентий Ризников", 8);
-        list.add(Person1);
-        list.add(Person2);
-        list.add(Person3);
-        list.add(Person4);
-        list.add(Person5);
+        list.add(new Person("Борис Васильев", 2));
+        list.add(new Person("Александра Булкина", 3));
+        list.add(new Person("Наталья Портамонова", 4));
+        list.add(new Person("Емельян Макаронов", 5));
+        list.add(new Person("Иннокентий Ризников", 6));
         return list;
     }
 }
